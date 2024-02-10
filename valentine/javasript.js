761afc43151b706c173c16d021c5a6490439d3c6
newnattapon395 <nattaponchumtim@gmail.com>
2024-02-10 16:00:33 +0700

    finish.

--- valentine/javasript.js
+++ valentine/javasript.js
@@ -4,3 +4,20 @@ function showButton() {
 
     window.alert("ไม่ใช่กดตรงนี้"); // เพิ่มข้อความแจ้งเตือน
 }
+
+
+function alert321() {
+    alert("แน่ใจอะป่าว");
+    alert("3");
+    alert("นั้นแหน่รีบกดละสิ");
+    alert("ยัง");
+    alert("ยัง!");
+    alert("ยัง ยังไม่หมดอีก");
+    alert("2");
+    alert("ใกล้ได้เห็นแล้ว");
+    alert("อีกนิด");
+    alert("นิด");
+    alert("นิ๊ดดด");
+    alert("1");
+    alert("Gooooo");
+}
